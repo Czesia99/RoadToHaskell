26a1e03e8ffe9bc44831c62396d7f2bef4c23d5e
czesia <yann.berger@outlook.com>
2021-04-11 07:02:01 +0200

    21 first tasks done

--- 01-day1/My.hs
+++ 01-day1/My.hs
@@ -7,55 +7,71 @@
 
 mySucc :: Int -> Int
 mySucc a = a + 1
+-- function which takes one Int as argument and returns its successor.
 
 myIsNeg :: Int -> Bool
 myIsNeg a
     | a < 0 = True
     | a >= 0 = False
+-- function which takes one Int as argument and returns True if it’s negative or False otherwise.
 
 myAbs :: Int -> Int
 myAbs a
     | a < 0 = a * (-1)
     | a >= 0 = a
+-- function which takes one Int as argument and returns it’s absolute value.
 
 myMin :: Int -> Int -> Int
 myMin a b
     | a - b <= 0  = a
     | b - a <= 0 = b
+-- function which takes two Ints as arguments and returns the minimum of the two.
 
 myMax :: Int -> Int -> Int
 myMax a b
     | a - b <= 0  = b
     | b - a <= 0 = a
+-- I’m sure you can guess what this function returns :)
 
 myTuple :: a -> b -> (a, b)
 myTuple a b = (a, b)
+-- function which takes two arguments and a return a tuple of those.
 
 myTruple :: a -> b -> c -> (a, b, c)
 myTruple a b c = (a, b, c)
+-- function which takes three arguments and a return a tuple of those.
 
 myFst :: (a, b) -> a
 myFst (a, b) = a
+-- function which takes a tuple as argument and returns its first value.
 
 mySnd :: (a, b) -> b
 mySnd (a, b) = b
+-- function which takes a tuple as argument and returns its second value.
 
 mySwap :: (a, b) -> (b, a)
 mySwap (a, b) = (b, a)
+-- function which takes a tuple as argument and returns a new tuple, with it’s two values swaped.
 
 myHead :: [a] -> a
 myHead [] = error "empty list"
 myHead (x:xs) = x
+-- function which takes a list as argument and returns its first value. 
+-- If the list is empty, an exception is raised with the function error.
 
 myTail :: [a] -> a
 myTail [] = error "empty list"
 myTail [x] = x
 myTail (_:xs) = myTail xs
+-- function which takes a list as argument and returns a new list without
+-- its first element. If the list is empty, an exception is raised with the function error.
 
 myLength :: [a] -> Int
 myLength [] = 0
 myLength [x] = 1
 myLength (x:xs) = 1 + myLength xs
+-- function which takes a list as argument and returns the number of
+-- elements in the list.
 
 myNth :: [a] -> Int -> a
 myNth [] n = error "empty list" 
@@ -64,6 +80,8 @@ myNth (x:xs) n
     | myLength (x:xs) < n = error "index out of range"
     | n == 0 = x
     | otherwise = myNth xs (n-1)
+-- function which takes a list and an Int (N) as argument and returns the element at index N in the list,
+-- or an error if the index is too large or negative.
 
 myTake :: Int -> [a] -> [a]
 myTake n [] = error "empty list"
@@ -72,6 +90,8 @@ myTake n (x:xs)
     | myLength (x:xs) < n = (x:xs) 
     | n == 1 = [x]
     | otherwise = x : myTake (n - 1) xs
+-- A function which takes an Int (N) and a list and returns a list with the Nth first elements of the list.
+-- If the list is too short the whole list is returned.
 
 myDrop :: Int -> [a] -> [a]
 myDrop n [] = error "empty list"
@@ -80,11 +100,40 @@ myDrop n (x:xs)
     | myLength (x:xs) < n = []
     | n == 1 = xs
     | otherwise = myDrop (n - 1) xs
+-- function which takes an Int (n) and a list and returns a list without the N first elements.
+-- If the list is tooshort an empty list is returned.
 
--- myAppend :: [a] -> [a] -> [a]
--- myAppend [] [a] = [a]
--- myAppend [a] [] = [a]
--- myAppend [] [] = []
--- myAppend (x:xs) (y:ys) = x
+myAppend :: [a] -> [a] -> [a]
+myAppend [] x = x
+myAppend (x:xs) y = x : myAppend xs y
+-- function which takes two lists and returns a new list with the second list appened to the first one.
 
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse [x] = [x]
+myReverse (x:xs) = myTail (x:xs) : (myReverse (myTake (myLength (x:xs) - 1) (x:xs)))
+-- function which takes a list and returns a list with all its elements in reverse order.
+
+myInit :: [a] -> [a]
+myInit [] = error "empty list"
+myInit [x] = []
+myInit (x:xs) = (myTake (myLength (x:xs) - 1) (x:xs))
+-- function which takes a list and returns a list with all its elements except the last one,
+-- or an error if the list is empty.
+
+myLast :: [a] -> a
+myLast [] = error "empty list"
+myLast [x] = x
+myLast (x:xs) = myNth (x:xs) (myLength (x:xs) - 1)
+-- function which takes a list and returns its last element, or an error if the list is empty.
+
+myZip :: [a] -> [b] -> [(a, b)]
+myZip [] _ = []
+myZip _ [] = []
+myZip (x:xs) (y:ys) = (x,y) : myZip xs ys
+-- function which takes two lists as arguments, and returns a list of tuples.
+-- The list produced is as long as the shortest list.
+
+myUnzip :: [(a, b)] -> ([a], [b])
+myUnzip [] = ([], [])
 

b18118972e05740767f9267b56371179cc96921b
yann <yann.berger@outlook.com>
2021-05-09 04:55:55 +0200

    argument managment getting better

--- 03-wolfram/wolfram.hs
+++ 03-wolfram/wolfram.hs
@@ -6,9 +6,12 @@
 --
 
 import Text.Read
+import Data.Maybe
 import System.Environment
 import System.Exit
 
+
+------ print functions ------
 printErrAndReturn :: String -> Int -> IO ()
 printErrAndReturn err i = do
     putStrLn err
@@ -22,60 +25,65 @@ printUsage = putStrLn "Usage:\n wolfram --rule n [--options]\n options:\n\t\
 \--window: the number of cells to display on each line (line width). If even,the central cell is displayed in the next cell on the right. The default value is 80.\n\t\
 \--move: a translation to apply on the window. If negative, the window is translated to the left. If positive, it’s translated to the right."
 
-checkArgs :: [(String, Int)] -> [Int]
-checkArgs args = [getRule args, getStart args, getLines args, getWindow args, getMove args]
+------ arguments management functions ------
+
+readInt :: String -> Maybe Int
+readInt = readMaybe
+
+makeTupleArgs :: [String] -> [(String, Int)]
+makeTupleArgs [] = []
+makeTupleArgs (x:y:xs) = (x, read y :: Int) : makeTupleArgs xs
+
+checkFlagValidity :: String -> Bool
+checkFlagValidity f
+    | f == "--rule" = True
+    | f == "--start" = True
+    | f == "--lines" = True
+    | f == "--window" = True
+    | f == "--move" = True
+    | otherwise = False
+
+checkFlagValue :: String -> Bool
+checkFlagValue v
+    | isNothing (readInt v) = False
+    | otherwise = True
+
+checkOptions :: [String] -> Bool
+checkOptions [] = True
+checkOptions (x:y:xs)
+    | checkFlagValidity x && checkFlagValue y = checkOptions xs
+    | otherwise = False
+
+getFlagValue :: [(String, Int)] -> String -> Int
+getFlagValue [] _ = 0
+getFlagValue ((x, y): xs) flag
+    | x == flag = y
+    | otherwise = getFlagValue xs flag
+
+getOptions :: [(String, Int)] -> [Int]
+getOptions args = [
+    getFlagValue args "--rule",
+    getFlagValue args "--start",
+    getFlagValue args "--lines",
+    getFlagValue args "--window",
+    getFlagValue args "--move"
+    ]
 
 wolfram :: [Int] -> IO ()
 wolfram [] = return ()
 wolfram [a] = return ()
 wolfram [rule, start, lines, window, move] = do
+    putStrLn "------ in wolfram ------"
     print rule
     print start
     print lines
     print window
     print move
 
--- readInt :: [Char] -> Maybe Int
--- readInt = readMaybe
-
-getRule :: [(String, Int)] -> Int
-getRule [] = 0
-getRule ((x, y): xs)
-    | x == "--rule" = y
-    | otherwise = getRule xs
-
-getStart :: [(String, Int)] -> Int
-getStart [] = 0
-getStart ((x, y): xs)
-    | x == "--start" = y
-    | otherwise = getStart xs
-
-getLines :: [(String, Int)] -> Int
-getLines [] = 0
-getLines ((x, y): xs)
-    | x == "--lines" = y
-    | otherwise = getLines xs
-
-getWindow :: [(String, Int)] -> Int
-getWindow [] = 80
-getWindow ((x, y): xs)
-    | x == "--window" = y
-    | otherwise = getWindow xs
-
-getMove :: [(String, Int)] -> Int
-getMove [] = 0
-getMove ((x, y): xs)
-    | x == "--move" = y
-    | otherwise = getMove xs
-
-makeTupleArgs :: [String] -> [(String, Int)]
-makeTupleArgs [] = []
-makeTupleArgs (x:y:xs) = (x, read y :: Int) : makeTupleArgs xs
-
-
 main :: IO ()
 main = do
     args <- getArgs
+    mapM_ putStrLn args
     case args of
         [] -> printUsage >> printErrAndReturn "Invalid number of arguments" 84
-        a -> wolfram (checkArgs (makeTupleArgs args))
+        a -> if checkOptions args then wolfram (getOptions (makeTupleArgs args)) else printUsage >> printErrAndReturn "Invalid arguments" 84

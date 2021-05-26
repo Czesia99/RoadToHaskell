b08b0f581a1bdeabb77f6600a91726efe4196875
yann <yann.berger@outlook.com>
2021-05-26 19:45:31 +0200

    trying things in Lib & reading things about k-means algorithm

--- 04-image_compressor/image-compressor/app/Main.hs
+++ 04-image_compressor/image-compressor/app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
 import Lib
+import System.Environment
+import System.Exit
+
+printUsage :: IO()
+printUsage = putStrLn "USAGE: ./imageCompressor n e IN\n\n\t\
+\n\tnumber of colors in the final image\n\t\
+\e\tconvergence limit\n\t\
+\IN\tpath to the file containing the colors of the pixels"
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    case args of
+        [] -> printUsage >> printErrAndReturn "Wrong number of arguments" 84
--- 04-image_compressor/image-compressor/src/Lib.hs
+++ 04-image_compressor/image-compressor/src/Lib.hs
@@ -1,6 +1,14 @@
 module Lib
-    ( someFunc
+    ( printErrAndReturn
+    , someFunc
     ) where
+
+import System.Exit
+
+printErrAndReturn :: String -> Int -> IO ()
+printErrAndReturn err i = do
+    putStrLn err
+    exitWith (ExitFailure i)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
